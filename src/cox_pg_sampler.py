279b5eab8ec399619a1b66e5f243233961a46ef8
shutech2001 <shutech2001@outlook.jp>
2025-04-08 14:25:05 +0900

    integrate tie and non-tie partial likelihood

--- src/cox_pg_sampler.py
+++ src/cox_pg_sampler.py
@@ -74,131 +74,6 @@ class CoxPGSampler:
         tilde_x_list: deque[np.ndarray] = deque()
         offset_list: deque[float] = deque()
         for event_num, event_time in zip(event_nums, event_times):
-            indices = risk_sets[event_time]
-            if len(indices) < 2:
-                # if only one person at risk, no contribution to the likelihood
-                continue
-
-            # calculate x_i^\top \beta
-            event_idx: int = indices[0]
-            event_linpred: float = self.covariates[event_idx, :].dot(beta)
-
-            # calculate \sum_{j \in R(t_i) \backslash \{i\}} exp(x_j^\top \beta)
-            at_risks_exclude_event_idx: np.ndarray = indices[1:]
-            at_risks_exclude_event_cov = self.covariates[at_risks_exclude_event_idx, :]
-            at_risks_exclude_event_linpred: np.ndarray = at_risks_exclude_event_cov.dot(beta)
-            exp_at_risks_exclude_event_linpred: np.ndarray = np.exp(at_risks_exclude_event_linpred)
-            sum_exp_at_risks_exclude_event_linpred: float = sum(exp_at_risks_exclude_event_linpred)
-
-            # calculate eta_i
-            eta_list.append(event_linpred - np.log(sum_exp_at_risks_exclude_event_linpred))
-
-            # local linearization for eta_i
-            # calculate \sum_{j \in R(t_i) \backslash \{i\}} exp(x_j^\top \beta_0)
-            at_risks_exclude_event_linpred_center: np.ndarray = at_risks_exclude_event_cov.dot(beta0)
-            exp_at_risks_exclude_event_linpred_center: np.ndarray = np.exp(at_risks_exclude_event_linpred_center)
-            # numerator is \sum_{j \in R(t_i) \backslash \{i\}} exp(x_j^\top \beta_0) x_j
-            numerator: np.ndarray = sum(
-                e * at_risks_exclude_event_cov[i, :] for i, e in enumerate(exp_at_risks_exclude_event_linpred_center)
-            )
-            # denominator is \sum_{j \in R(t_i) \backslash \{i\}} exp(x_j^\top \beta_0)
-            denominator: float = sum(exp_at_risks_exclude_event_linpred_center)
-
-            # calculate \tilde{x}_i
-            tilde_x_list.append(self.covariates[event_idx, :] - (numerator/denominator))
-
-            # calculate offset
-            sum_exp_at_risks_exclude_event_linpred_center: float = sum(exp_at_risks_exclude_event_linpred_center)
-            offset_list.append(
-                np.log(sum_exp_at_risks_exclude_event_linpred_center) - (numerator / denominator).dot(beta0)
-            )
-
-        return eta_list, tilde_x_list, offset_list
-
-    def compute_event_contribution_efron(
-        self,
-        beta0: np.ndarray,
-        beta: np.ndarray,
-        risk_sets: dict[float, np.ndarray],
-        event_times: np.ndarray,
-        event_nums: deque[int]
-    ) -> tuple[deque[float], deque[np.ndarray], deque[float]]:
-        eta_list: deque[float] = deque()
-        tilde_x_list: deque[np.ndarray] = deque()
-        offset_list: deque[float] = deque()
-        for event_num, event_time in zip(event_nums, event_times):
-            indices: np.ndarray = risk_sets[event_time]
-            if len(indices) < 2:
-                # if only one person at risk, no contribution to the likelihood
-                continue
-
-            # calculate \sum_{j\i\in d(t)} \exp(x_i^\top \beta)
-            event_idxs: np.ndarray = indices[:event_num]
-            event_cov: np.ndarray = self.covariates[event_idxs, :]
-            event_linpred: np.ndarray = event_cov.dot(beta)
-            exp_event_linpred: np.ndarray = np.exp(event_linpred)
-            sum_exp_event_linpred: float = sum(exp_event_linpred)
-
-            # calculate \sum_{j\in R(t)} \exp(x_j^\top \beta)
-            at_risks_cov: np.ndarray = self.covariates[indices, :]
-            at_risks_linpred: np.ndarray = at_risks_cov.dot(beta)
-            exp_at_risks_linpred: np.ndarray = np.exp(at_risks_linpred)
-            sum_exp_at_risks_linpred: float = sum(exp_at_risks_linpred)
-
-            # local linearization
-            event_linpred_center: np.ndarray = event_cov.dot(beta0)
-            exp_event_linpred_center: np.ndarray = np.exp(event_linpred_center)
-            sum_exp_event_linpred_center: float = sum(exp_event_linpred_center)
-
-            at_risks_linpred_center: np.ndarray = at_risks_cov.dot(beta0)
-            exp_at_risks_linpred_center: np.ndarray = np.exp(at_risks_linpred_center)
-            sum_exp_at_risks_linpred_center: float = sum(exp_at_risks_linpred_center)
-
-            for event_accumulated_num, event_idx in enumerate(event_idxs):
-                # x_{t,l}^\top\beta
-                one_event_cov: np.ndarray = self.covariates[event_idx, :]
-                one_event_linpred: float = one_event_cov.dot(beta)
-                exp_one_event_linpred: float = np.exp(one_event_linpred)
-                R: float = sum_exp_at_risks_linpred - (event_accumulated_num*sum_exp_event_linpred/event_num)
-                print(one_event_linpred - np.log(R-exp_one_event_linpred))
-                eta_list.append(one_event_linpred - np.log(R-exp_one_event_linpred))
-
-                # approximiate eta
-                one_event_linpred_center: float = one_event_cov.dot(beta0)
-                exp_one_event_linpred_center: float = np.exp(one_event_linpred_center)
-
-                sum_weighted_exp_at_risks_linpred_center: np.ndarray = sum(
-                    e * at_risks_cov[i, :] for i, e in enumerate(exp_at_risks_linpred_center)
-                )
-                sum_weighted_exp_event_linpred_center: np.ndarray = sum(
-                    e * event_cov[i, :] for i, e in enumerate(exp_event_linpred_center)
-                )
-                weighted_exp_one_event_linpred_center: np.ndarray = exp_one_event_linpred_center * one_event_cov
-
-                denominator: float = sum_exp_at_risks_linpred_center - event_accumulated_num*sum_exp_event_linpred_center/event_num - exp_one_event_linpred_center  # noqa: E501
-
-                tilde_x_list.append(
-                    one_event_cov - ((sum_weighted_exp_at_risks_linpred_center - event_accumulated_num*sum_weighted_exp_event_linpred_center/event_num - weighted_exp_one_event_linpred_center)/denominator)  # noqa: E501
-                )
-
-                offset_list.append(
-                    np.log(denominator) - ((sum_weighted_exp_at_risks_linpred_center - event_accumulated_num*sum_weighted_exp_event_linpred_center/event_num - weighted_exp_one_event_linpred_center)/denominator).dot(beta0)  # noqa: E501
-                )
-
-        return eta_list, tilde_x_list, offset_list
-
-    def compute_event_contribution_breslow(
-        self,
-        beta0: np.ndarray,
-        beta: np.ndarray,
-        risk_sets: dict[float, np.ndarray],
-        event_times: np.ndarray,
-        event_nums: deque[int]
-    ) -> tuple[deque[float], deque[np.ndarray], deque[float]]:
-        eta_list: deque[float] = deque()
-        tilde_x_list: deque[np.ndarray] = deque()
-        offset_list: deque[float] = deque()
-        for event_num, event_time in zip(event_nums, event_times):
             indices: np.ndarray = risk_sets[event_time]
             if len(indices) < 2:
                 # if only one person at risk, no contribution to the likelihood
@@ -276,7 +151,7 @@ class CoxPGSampler:
         risk_sets, event_times, event_nums = self.build_risk_sets(time, event)
 
         for it in tqdm(range(n_iter)):
-            eta_list, tilde_x_list, offset_list = self.compute_event_contribution_breslow(
+            eta_list, tilde_x_list, offset_list = self.compute_event_contribution(
                 beta0, beta, risk_sets, event_times, event_nums
             )
             # sampling from Poly\'a gamma distribution
